384fff46bdf856829809313763a721c6e8137f69
zhli <zhli@mdl.com>
2016-12-19 18:34:38 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,70 +11,72 @@ var filePath = {
     fonts: assetsPath + 'fonts*/**/*',
     rev: distPath + 'rev/'
 };
+var env = process.env.NODE_ENV || 'dev';
+var isProduction = env === 'production';
 
 
 gulp.task('style', function () {
     return gulp.src(filePath.sass)
         .pipe(plugins.plumber({
             errorHandler: function(err) {
-                console.log(err);
                 this.emit('end') }
         }))
         .pipe(plugins.sass({
             outputStyle: 'compressed'
         }))
         .pipe(plugins.autoprefixer())
-        .pipe(plugins.rev())
+        .pipe(plugins.if( isProduction, plugins.rev() ))
         .pipe(gulp.dest(distPath + 'css'))
-        .pipe(plugins.rev.manifest())
-        .pipe(gulp.dest(filePath.rev + 'css'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(plugins.if( isProduction, plugins.rev.manifest() ))
+        .pipe(plugins.if(isProduction, gulp.dest(filePath.rev + 'css') ));
 });
 
 gulp.task('script', function () {
     return gulp.src(filePath.js)
         .pipe(plugins.plumber({
             errorHandler: function(err) {
-                console.log(err);
                 this.emit('end') }
         }))
-        .pipe(plugins.rev())
+        .pipe(plugins.if( isProduction, plugins.rev() ))
         .pipe(plugins.uglify())
         .pipe(gulp.dest(distPath + 'js'))
-        .pipe(plugins.rev.manifest())
-        .pipe(gulp.dest(filePath.rev + 'js'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(plugins.if( isProduction, plugins.rev.manifest() ))
+        .pipe(plugins.if(isProduction, gulp.dest(filePath.rev + 'js') ));
 });
 
 gulp.task('template', function () {
     return gulp.src(filePath.views)
         .pipe(plugins.swig({defaults: { cache: false }}))
-        .pipe(gulp.dest(distPath + 'views'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(gulp.dest(distPath + 'views'));
 });
 
 gulp.task('fonts', function () {
-    return gulp.src(filePath.fonts)
-        .pipe(plugins.rev())
-        .pipe(gulp.dest(distPath))
-        .pipe(plugins.rev.manifest())
-        .pipe(gulp.dest(filePath.rev + 'fonts'))
+    var src = gulp.src(filePath.fonts);
+    src = isProduction ?
+        src.pipe(plugins.if( isProduction, plugins.rev() ))
+        : src;
+    return src.pipe(gulp.dest(distPath))
+        .pipe(plugins.if( isProduction, plugins.rev.manifest() ))
+        .pipe(plugins.if(isProduction, gulp.dest(filePath.rev + 'fonts') ));
 });
 
 gulp.task('images', function () {
-    return gulp.src(filePath.images)
+    var src = gulp.src(filePath.images);
+    src = isProduction ? src
         .pipe(plugins.imagemin({
             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
             progressive: true //类型：Boolean 默认：false 无损压缩jpg图片
         }))
-        .pipe(plugins.rev())
-        .pipe(gulp.dest(distPath))
-        .pipe(plugins.rev.manifest())
-        .pipe(gulp.dest(filePath.rev + 'images'))
+        .pipe(plugins.if( isProduction, plugins.rev() ))
+        : src;
+    return src.pipe(gulp.dest(distPath))
+        .pipe(plugins.if( isProduction, plugins.rev.manifest() ))
+        .pipe(plugins.if(isProduction, gulp.dest(filePath.rev + 'image') ));
 });
 
 gulp.task('server', function () {
     browserSync.init({
+        open: false,
         server: {
             baseDir: [".", "./dist/views"],
             index: "/dist/views/home/index.html",
@@ -82,18 +84,28 @@ gulp.task('server', function () {
         },
         port: 9000
     });
+    console.log('App (dev) is going to be running on http://localhost:9000 (by browsersync).');
 });
 
 gulp.task('watch', function () {
-    gulp.watch(assetsPath + '**/*', ['build']);
+    gulp.watch(assetsPath + '**/*', ['reload']);
+});
+
+gulp.task('clean', function() {
+    return gulp.src(distPath).pipe(plugins.clean());
+});
+
+gulp.task('reload', ['build'], function() {
     browserSync.reload();
 });
 
 gulp.task('build', ['images', 'fonts', 'script', 'style', 'template'], function() {
+    var src = gulp.src(['./dist/rev/**/*.json', './dist/views*/**', './dist/css*/**']);
     //- 读取 rev-manifest.json 文件以及需要进行文件替换
-    return gulp.src(['./dist/rev/**/*.json', './dist/views*/**', './dist/css*/**'])
-        .pipe(plugins.revCollector())
-        .pipe(gulp.dest(distPath));
+    return isProduction ?
+        src.pipe(plugins.revCollector())
+            .pipe(gulp.dest(distPath))
+        : src;
 });
 
 gulp.task('default', ['build', 'watch'], function () {
